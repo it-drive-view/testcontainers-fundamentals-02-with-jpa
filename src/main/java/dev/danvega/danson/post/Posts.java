d46c17626308427d052c59e96ad24b05c36823b3
it-drive-view <it.drive.view@gmail.com>
2024-02-10 14:43:01 +0100

    wip

--- src/main/java/dev/danvega/danson/post/Posts.java
+++
@@ -1,6 +0,0 @@
-package dev.danvega.danson.post;
-
-import java.util.List;
-
-public record Posts(List<Post> posts) {
-}
---
+++ src/test/java/dev/danvega/danson/post/FirstTest.java
@@ -0,0 +1,38 @@
+package dev.danvega.danson.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class FirstTest {
+
+    @Container
+    static PostgreSQLContainer database = new PostgreSQLContainer("postgres:12")
+            .withDatabaseName("springboot")
+            .withPassword("springboot")
+            .withUsername("springboot");
+
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry propertyRegistry) {
+        propertyRegistry.add("spring.datasource.url", database::getJdbcUrl);
+        propertyRegistry.add("spring.datasource.password", database::getPassword);
+        propertyRegistry.add("spring.datasource.username", database::getUsername);
+    }
+
+    @Test
+    void shouldReturnOrdersThatContainMacBookPro() {
+    }
+
+
+
+
+
+}
