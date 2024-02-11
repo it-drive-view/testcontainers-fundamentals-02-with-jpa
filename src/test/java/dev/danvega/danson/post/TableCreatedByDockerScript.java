4e4c6b2a670179d099d32d367f64cd308e0070ef
it-drive-view <it.drive.view@gmail.com>
2024-02-11 03:54:31 +0100

    wip

--- src/test/java/dev/danvega/danson/post/TableCreatedByDockerScript.java
+++ src/test/java/dev/danvega/danson/post/TableCreatedByDockerScript.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -16,27 +18,27 @@ import java.util.Optional;
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class FirstTest {
+public class TableCreatedByDockerScript {
 
     @Container
-    static PostgreSQLContainer database = new PostgreSQLContainer("postgres:12")
+    static GenericContainer database = new PostgreSQLContainer("postgres:12")
             .withDatabaseName("springboot")
             .withPassword("springboot")
-            .withUsername("springboot");
+            .withUsername("springboot")
+            .withClasspathResourceMapping("PostgresInit.sql", "/docker-entrypoint-initdb.d/script.sql", BindMode.READ_ONLY);
 
     @Autowired
     PostRepository postRepository;
 
     @DynamicPropertySource
     static void setDatasourceProperties(DynamicPropertyRegistry propertyRegistry) {
-        propertyRegistry.add("spring.datasource.url", database::getJdbcUrl);
-        System.out.println("===> " + database.getJdbcUrl());
-        propertyRegistry.add("spring.datasource.password", database::getPassword);
-        propertyRegistry.add("spring.datasource.username", database::getUsername);
+        propertyRegistry.add("spring.datasource.url", () -> ((PostgreSQLContainer) database).getJdbcUrl());
+        System.out.println("===> " +  ((PostgreSQLContainer) database).getJdbcUrl());
+        propertyRegistry.add("spring.datasource.username", () -> ((PostgreSQLContainer) database).getUsername());
     }
 
     @Test
-    void shouldReturnOrdersThatContainMacBookPro() {
+    void shouldReturnOrdersThatContainMacBookPro() throws InterruptedException {
         Post post = new Post(1,1,"title","body",1);
         postRepository.save(post);
         Optional<Post> byId = postRepository.findById(1);
