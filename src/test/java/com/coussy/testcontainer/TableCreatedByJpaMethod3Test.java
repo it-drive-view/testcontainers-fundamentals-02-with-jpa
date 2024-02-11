9b49873e15c94e949d3f77154329cb75cc1eb3ba
it-drive-view <it.drive.view@gmail.com>
2024-02-11 17:54:08 +0100

    wip

---
+++ src/test/java/com/coussy/testcontainer/TableCreatedByJpaMethod3Test.java
@@ -0,0 +1,56 @@
+package com.coussy.testcontainer;
+
+import com.coussy.testcontainer.post.Post;
+import com.coussy.testcontainer.post.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+// 1- we need to create "post" table in the database
+// 2- we are in jpa/hibernate, so with @Entity annotation, program will be able to create the table
+// 3- but we have to specify the property : "spring.jpa.hibernate.ddl-auto= create-drop"
+// 4- this will be done by the property file : test-override.properties
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class TableCreatedByJpaMethod3Test {
+
+    @Container
+    static GenericContainer database = new PostgreSQLContainer("postgres:12")
+            .withDatabaseName("springboot")
+            .withPassword("springboot")
+            .withUsername("springboot");
+
+    @Autowired
+    PostRepository postRepository;
+
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry propertyRegistry) {
+        propertyRegistry.add("spring.datasource.url", () -> ((PostgreSQLContainer) database).getJdbcUrl());
+        propertyRegistry.add("spring.datasource.username", () -> ((PostgreSQLContainer) database).getUsername());
+    }
+
+    @Test
+    void shouldReturnOrdersThatContainMacBookPro() throws InterruptedException {
+        Post post = new Post(1,1,"title","body",1);
+        postRepository.save(post);
+        Optional<Post> byId = postRepository.findById(1);
+        Assertions.assertTrue(byId.isPresent());
+    }
+
+}
