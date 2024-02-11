4eaa43dafcb719e7bcbb11626bb223da86444201
it-drive-view <it.drive.view@gmail.com>
2024-02-11 12:21:32 +0100

    wip

---
+++ src/test/java/UsingNewAnnotation/TableCreatedByJpa.java
@@ -0,0 +1,49 @@
+package UsingNewAnnotation;
+
+import dev.danvega.danson.post.Post;
+import dev.danvega.danson.post.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+@DataJpaTest(properties = {
+        "spring.jpa.hibernate.ddl-auto= create-drop"
+})
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TableCreatedByJpa {
+
+    @Container
+    static GenericContainer database = new PostgreSQLContainer("postgres:12")
+            .withDatabaseName("springboot")
+            .withPassword("springboot")
+            .withUsername("springboot");
+
+    @Autowired
+    PostRepository postRepository;
+
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry propertyRegistry) {
+        propertyRegistry.add("spring.datasource.url", () -> ((PostgreSQLContainer) database).getJdbcUrl());
+        propertyRegistry.add("spring.datasource.username", () -> ((PostgreSQLContainer) database).getUsername());
+    }
+
+    @Test
+    void shouldReturnOrdersThatContainMacBookPro() throws InterruptedException {
+        Post post = new Post(1,1,"title","body",1);
+        postRepository.save(post);
+        Optional<Post> byId = postRepository.findById(1);
+        Assertions.assertTrue(byId.isPresent());
+    }
+
+}
